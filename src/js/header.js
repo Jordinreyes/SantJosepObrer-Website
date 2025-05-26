ff622d58372f39a0d71f40cc24dffd3902d8c782
Jordin Reyes <jordinp062@gmail.com>
2025-05-26 20:55:56 +0200

    Home casi terminada

--- src/js/header.js
+++ src/js/header.js
@@ -1,3 +1,6 @@
+/**
+ * INICIO HEADER CAMBIO DE COLOR
+ */
 const header = document.getElementById("header");
 const main = document.getElementById("main");
 
@@ -18,8 +21,6 @@ window.addEventListener("scroll", () => {
   if (currentScroll >= main.offsetTop - header.offsetHeight) {
     header.classList.remove("header-hidden");
     header.classList.add("header-white");
-
-    // Cambiar imagen del logo
     const logo = header.querySelector("img");
     logo.src = "public/logo/image2.webp";
 
@@ -28,12 +29,13 @@ window.addEventListener("scroll", () => {
 
   lastScroll = currentScroll;
 });
+/**
+ * TERMINA HEADER CAMBIO DE COLOR
+ */
 
 /**
  * MENU DE NAVEGACIÓN
  */
-// Menú móvil
-// Menú móvil
 document.getElementById("hamburger").addEventListener("click", () => {
   document.getElementById("mobile-menu").classList.remove("-translate-x-full");
 });
@@ -42,7 +44,6 @@ document.getElementById("close-menu").addEventListener("click", () => {
   document.getElementById("mobile-menu").classList.add("-translate-x-full");
 });
 
-// Submenús móviles (solo con el icono)
 document.querySelectorAll(".submenu-toggle").forEach((toggle) => {
   const icon = toggle.querySelector("i");
   icon.addEventListener("click", (e) => {
@@ -52,7 +53,6 @@ document.querySelectorAll(".submenu-toggle").forEach((toggle) => {
   });
 });
 
-// Submenús desktop (solo con el icono)
 document.querySelectorAll(".submenu-desktop-toggle").forEach((toggle) => {
   const icon = toggle.querySelector("i");
   icon.addEventListener("click", (e) => {
@@ -65,9 +65,23 @@ document.querySelectorAll(".submenu-desktop-toggle").forEach((toggle) => {
   });
 });
 
-// Cierra los submenús si haces click fuera
 document.addEventListener("click", (e) => {
   if (!e.target.closest(".submenu-desktop-toggle")) {
     document.querySelectorAll(".submenu-desktop-toggle + ul").forEach((ul) => ul.classList.add("hidden"));
   }
 });
+/**
+ * TERMINA MENU DE NAVEGACIÓN
+ */
+
+/**
+ * ALERT CAROSELL
+ */
+if (window.innerWidth < 768) {
+  window.addEventListener("load", function () {
+    alert("Desliza la imagen para ver más contenido educativo.");
+  });
+}
+/**
+ * TERNINA ALERT CAROUSEL
+ */
